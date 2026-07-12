14007b4fdcd7b9ad19f45a9e6b4ee5879ce9552e
akash goud goriga <akashgoud.goriga1975@gmail.com>
2026-07-12 21:09:55 +0530

    code completed

---
+++ code.py
@@ -0,0 +1,49 @@
+import random
+options = ["rock", "paper", "scissor"]
+
+print("welcome to rock paper scissor game")
+while True:
+    cump = random.choice(options)
+    print("type 'q' or 'Q' to exit the game")
+
+
+    hum = input("enter rock or paper or scissor :-  ")
+    
+    if hum == cump:
+        print(f"computer's choice {cump} , your choice {hum}")
+        print("nobody won")
+        break
+    elif hum == "rock" and cump == "scissor":
+        print(f"computer's choice : {cump} , your's choice {hum} ")
+        print("congratulations you won")
+        break
+    elif hum == "scissor" and cump == "paper":
+        print(f"computer's choice {cump} , your choice {hum}")  
+        print("congratulations you won")
+        break  
+    elif hum == "paper" and cump == "rock":
+        print(f"computer's choice {cump} , your choice {hum}")
+        print("congratulations you won")
+        break
+    elif hum == "rock" and cump == "paper":
+        print(f"computer's choice {cump} , your choice {hum}")
+        print("computer won")
+        break
+    elif hum == "scissor" and cump == "rock":
+        print(f"computer's choice {cump} , your choice {hum}")
+        print("computer won")
+        break
+    elif hum == "paper" and cump == "scissor":
+        print(f"computer's choice {cump} , your choice {hum}")
+        print("computer won")
+        break
+    elif hum == "q" or "Q":
+        print("exiting the game, thank you for playing")
+        break
+    else:
+        print("error")
+        
+        
+
+
+      
